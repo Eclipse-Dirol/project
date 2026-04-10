d7644b292154ce7a8b7c878b781fe5c5cc2b8562
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-04-10 21:09:13 +0500

    rework CLI to be more beautiful

--- House prices/CLI.py
+++ House prices/CLI.py
@@ -1,6 +1,7 @@
 from config import config
 import pandas as pd
 import itertools
+import os
 
 class CLI():
     def __init__(self):
@@ -10,54 +11,41 @@ class CLI():
     def __call__(self):
         return self.forward()
 
-    def choice_train_test_val(self) -> tuple[pd.DataFrame, bool, bool, bool, bool]:
+    def all_in_one(self) -> tuple[pd.DataFrame, bool, bool, bool, bool, list]:
+        setting = {'Train': False, 'Test': False, 'Submission': False, 'FE': False}
+        selected_models = []
+        all_model_phrase = 'all model'
         df = None
-        use_train = use_test = use_submit = fe = False
-        answer = int(input(f'Choice what you want: \n 1) {self.model_mode[0]} \n 2) {self.model_mode[1]}(if model have weight) \n 3) {self.model_mode[2]}(if model have weight) \n you answer: '))
-        if answer == 1:
-            if config.path.train == '': raise ValueError('path not found')
-            df = pd.read_csv(config.path.train)
-            fe_answer = int(input('Do you want to do FE?\n 1) Yes\n 2) No\nyou choose:'))
-            if fe_answer == 1:
-                fe = True
-            use_train = True
-        elif answer == 2:
-            if config.path.train == '': raise ValueError('path not found')
-            df = pd.read_csv(config.path.train)
-            fe_answer = int(input('Do you want to do FE?\n 1) Yes\n 2) No\nyou choose:'))
-            if fe_answer == 1:
-                fe = True
-            use_test = True
-        elif answer == 3:
-            if config.path.test == '': raise ValueError('path not found')
-            df = pd.read_csv(config.path.test)
-            use_submit = True
-        return (df, use_train, use_test, use_submit, fe)
-
-    def choice_model(self) -> list:
-        models = pd.DataFrame({
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f'you selected models: {selected_models}')
+            print('-----setting-----')
+            for num, (key, value) in enumerate(setting.items(), 1):
+                print(f'{num}) {key} | {'ON' if value else 'OFF'}')
+            models = pd.DataFrame({
             self.type_models[0]: pd.Series(config.models[self.type_models[0]]),
             self.type_models[1]: pd.Series(config.models[self.type_models[1]])
         }).fillna('')
-        print('====================================================================')
-        print(models)
-        print('Enter your answer in the format: column row\n(If you want to exit, enter: "exit", if you want to add all models, enter: "all model")')
-        selected_models = []
-        exit_phrase = 'exit'
-        all_model_phrase = 'all model'
-        while True:
-            input_ = input()
-            if input_ == all_model_phrase:
-                return list(itertools.chain.from_iterable(config.models.values()))
-            if input_ == exit_phrase:
-                return selected_models
-            cell = [int(x) for x in input_.split()]
-            column, row = cell[0], cell[1]
-            selected_models.append(models.iloc[row, column])
-            print(f'you selected models: {selected_models}')
+            print('=====m - select model=====q - quit=====')
+            print(models)
+            cmd = input(">> ").lower()
+            if cmd in ['1', '2', '3', '4']:
+                key = list(setting.keys())[int(cmd)-1]
+                setting[key] = not setting[key]
+            elif cmd == all_model_phrase:
+                selected_models.extend(itertools.chain.from_iterable(config.models.values()))
+            elif cmd == 'm':
+                idx = input("Индекс модели column row: ")
+                cell = [int(x) for x in idx.split()]
+                selected_models.append(models.iloc[cell[1], cell[0]])
+            elif cmd == 'q':
+                if setting['Train'] or setting['Test']:
+                    if config.path.train is '': raise ValueError('path not found')
+                    df = pd.read_csv(config.path.train)
+                if setting['Submission']:
+                    if config.path.train is '': raise ValueError('path not found')
+                    df = pd.read_csv(config.path.test)
+                return (df, setting['Train'], setting['Test'], setting['Submission'], setting['FE'], selected_models)
 
-    def forward(self) -> tuple[tuple[pd.DataFrame, bool, bool, bool, bool], list]:
-        df, train, test, use_submit, fe = self.choice_train_test_val()
-        list_of_models = self.choice_model()
-        output = (df, train, test, use_submit, fe)
-        return (output, list_of_models)
+    def forward(self) -> tuple[pd.DataFrame, bool, bool, bool, bool, list]:
+        return self.all_in_one()
