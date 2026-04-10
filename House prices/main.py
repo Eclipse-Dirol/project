e5133ffe739248656f53f7d48807906b5c99f642
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-04-10 21:12:45 +0500

    reworked main, collected full pipeline

--- House prices/main.py
+++ House prices/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from config import config
 from preprocessing import work
 from models.linear_models import linerreg, SVR, lassocv, ridgecv, elasticnetcv
-from models.tree_models import dectree, random_forest, catboost, xgboost
+from models.tree_models import decisiontree, random_forest, catboost, xgboost
 from CLI import CLI
 
 cli = CLI()
@@ -11,8 +11,7 @@ prep = work()
 
 def main():
     output = {}
-    choice_with_df, list_of_model = cli()
-    df, train, test, use_submit, fe = choice_with_df
+    df, train, test, use_submit, fe, model_list = cli()
     print('===================start prep===================')
     df, target = prep.prep(
         df = df,
@@ -22,12 +21,17 @@ def main():
     )
     print('===================start train===================')
     if train:
-        for name_model in list_of_model:
+        for name_model in model_list:
             model_class = globals().get(name_model)
             model = model_class()
             metrics = model.train(X_train = df, y = target, folds = config.args.kfold.folds, repeat = config.args.kfold.repeat)
             output[name_model] = metrics
-    print(output['lassocv']['mae'][0])
+    for model_name in output.keys():
+        metrics_model = output[model_name]
+        print(f'====={model_name}=====')
+        print(f'MSE: {metrics_model['mse'][0]} | MSE_std: {metrics_model['mse'][0]}')
+        print(f'MAE: {metrics_model['mae'][0]} | MAE_std: {metrics_model['mae'][0]}')
+        print(f'R2: {metrics_model['r2']}')
     
     
 if __name__ == '__main__':
